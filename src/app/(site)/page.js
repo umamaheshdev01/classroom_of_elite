2bda314680a13938647bc8064198b6d3b4225433
Mahesh <uma@gmail.com>
2024-03-02 20:01:12 +0530

    ok

---
+++ src/app/(site)/page.js
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+
+
+export default function Home() {
+  return (
+    <>
+    
+    <h1 className="text-green-500">Home Page</h1>
+  
+    
+    </>
+  );
+}
