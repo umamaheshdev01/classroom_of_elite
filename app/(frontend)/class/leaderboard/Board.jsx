6637c8501a541aa79a89f1aa5669f2e0c90c8a6b
sakshampandey03 <kumarsaksham224@gmail.com>
2024-03-07 17:58:34 +0530

    leaderboard ui

--- app/(frontend)/class/leaderboard/Board.jsx
+++ app/(frontend)/class/leaderboard/Board.jsx
@@ -4,16 +4,17 @@ const Board = ({ users }) => {
     const sortedUsers = users.sort((a, b) => b.score - a.score);
   
     return (
-      <div className="bg-gray-200 min-h-screen w-1/2 mx-auto p-8">
-        <h1 className="text-4xl font-bold mb-8 text-center">Leaderboard</h1>
-        <div className="flex flex-col">
+      <div className="bg-black min-h-screen w-4/5 mx-auto p-8">
+        <h1 className="text-4xl font-bold mb-8 text-center ">Leaderboard</h1>
+        <hr className="border-white pb-3 w-3/4 mx-auto"></hr>
+        <div className="flex flex-col px-10">
           {sortedUsers.map((user, index) => (
-            <div key={index} className="bg-white p-6 shadow-md flex flex-row
+            <div key={index} className="p-2 border-white border-2  shadow shadow-white-500/40 hover:shadow-white-500/40 flex flex-row
             justify-around m-3 rounded-xl">
-                <span className="font-bold"> {index+1} </span>
-            <span> <img src='C:\Users\kumar\OneDrive\Desktop\leaderboard2\leaderboard\src\components\GGSJ_poster.png' className="border-black rounded-full w-[30px]"></img></span>
-             <span> <h3 className="text-2xl font-semibold mb-2">{user.name}</h3></span> 
-             <span> <p className="text-gray-600">XP: {user.score}</p></span>
+                <span className="font-bold text-3xl"> {index+1} </span>
+            <span> <img src='https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?w=740&t=st=1709813753~exp=1709814353~hmac=c5d99d40c3b3771cdc17791208cbb8dc434fd828ba63f77df4d6314cc231a5a7' className="border-black rounded-full w-[60px]"></img></span>
+             <span> <h3 className="text-2xl mb-2">{user.name}</h3></span> 
+             <span> <p className="text-white-600 font-semibold">XP: {user.score}</p></span>
             </div>
           ))}
         </div>
