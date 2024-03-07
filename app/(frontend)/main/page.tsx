d9dad8bb02f0dca4b51e429dabb2247af5b9a54e
Mahesh <uma@gmail.com>
2024-03-07 12:58:55 +0530

    ok

--- app/(frontend)/main/page.tsx
+++ app/(frontend)/main/page.tsx
@@ -45,8 +45,26 @@ export default function BentoGridDemo() {
   }, [sessionData, sessionStatus]);
 
   return (
+
+
     
-    <>{data && <BentoGrid className="max-w-4xl mx-auto my-4">
+    
+    <>
+    <div className="ml-[220px] mt-4">
+    <button className="relative inline-flex mr-5 h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          Create
+        </span>
+      </button>
+      <button className="relative  inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <span className="absolute pl-4 pr-4 inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          Join
+        </span>
+      </button>
+    </div>
+    {data && <BentoGrid className="max-w-4xl mx-auto my-4">
     {data.map((item,i) => (
       <BentoGridItem
         key={i}
