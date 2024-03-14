520ee3b474edc546f49a0f01f966605518f05b80
Mahesh <uma@gmail.com>
2024-03-14 19:34:28 +0530

    ok

--- app/(frontend)/class/leader/board.jsx
+++ app/(frontend)/class/leader/board.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Board = ({ users }) => {
 
-    const sortedUsers = users.sort((a, b) => b.score - a.score);
+    const sortedUsers = users.sort((a, b) => b.xp - a.xp);
   
     return (
       <div className="bg-black min-h-screen w-4/5 mx-auto p-8">
@@ -12,9 +12,9 @@ const Board = ({ users }) => {
             <div key={index} className="p-2 border-white border-2  shadow shadow-white-500/40 hover:shadow-white-500/40 flex flex-row
             justify-around m-3 rounded-xl">
                 <span className="font-bold text-3xl"> {index+1} </span>
-            <span> <img src='https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?w=740&t=st=1709813753~exp=1709814353~hmac=c5d99d40c3b3771cdc17791208cbb8dc434fd828ba63f77df4d6314cc231a5a7' className="border-black rounded-full w-[60px]"></img></span>
-             <span> <h3 className="text-2xl mb-2">{user.name}</h3></span> 
-             <span> <p className="text-white-600 font-semibold">XP: {user.score}</p></span>
+            <span> <img src={user.Users.image} className="border-black rounded-full w-[60px]"></img></span>
+             <span> <h3 className="text-2xl mb-2">{user.Users.fname}</h3></span> 
+             <span> <p className="text-white-600 font-semibold">XP: {user.xp}</p></span>
             </div>
           ))}
         </div>
--- app/(frontend)/class/leader/page.jsx
+++ app/(frontend)/class/leader/page.jsx
@@ -1,28 +1,32 @@
+'use client'
+import { useEffect, useState } from 'react';
 import Board from './board';
 export default function App() {
-  const users =  [{name: "Tj", score: 1},
-          {name: "Chris", score: 69},
-          {name: "Dave", score: 17},
-          {name: "Ben", score: 11},
-          {name: "Caty", score: 21},
-          {name: "Miller", score: 33},
-          {name: "Zack", score: 88},
-          {name: "Sam", score: 42},
-          {name: "Nicky", score: 22},
-          {name: "Cheyenne", score: 55},
-          {name: "Adela", score: 72},
-          {name: "Wongo", score: 35},
-          {name: "Brett", score: 98},
-          {name: "Gina", score: 4},
-          {name: "Allen", score: 7},
-          {name: "Matt", score: 46},
-          {name: "Amanda", score: 31},
-          {name: "Jamie", score: 100},
-          {name: "Sarah", score: 56},
-          {name: "Owen", score: 45}]
+   
+    const [data,setData] = useState(null)
+
+    useEffect(()=>{
+        
+        const code = localStorage.getItem('class')
+      
+        fetch(`http://localhost:3000/api/member?id=${code}`)
+        .then(res=>res.json()).then(data=>{
+            const k =data.data
+            // const arr = k.filter((item)=>item.role==0)
+         const arr = k
+        setData(arr)
+        })
+
+ 
+        
+
+    })
+
+
+
   return (
     <div>
-      <Board users={users} />
+     {data && <Board users={data} />}
     </div>
   );
 }
